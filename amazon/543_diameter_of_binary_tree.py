e5d3381e9449a58ec3b6ad7f40d36bb05ac0c4a0
ALJDBS <70330110+TheSpawnal@users.noreply.github.com>
2025-10-08 23:55:32 +0200

    Update 543_diameter_of_binary_tree.py

--- amazon/543_diameter_of_binary_tree.py
+++ amazon/543_diameter_of_binary_tree.py
@@ -24,3 +24,59 @@
 #         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        def diameter(node, res):
+            if not node:
+                return 0
+            
+            #recursively calculate the diameter of left and right subtrees
+            left = diameter(node.left, res)
+            right = diameter(node.right, res)
+            #update max diameter encoutered 
+            res[0] = max(res[0], left + right)
+            return max(left, right)+1
+        res =[0]
+        diameter(root, res)
+        return res[0]
+    
+#alternative: 
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        def getMaxLengthOfSubTree(root):
+            queue = deque([root])
+            maxLength = 0
+            while(queue):
+                for i in range(len(queue)):
+                    node = queue.popleft()
+                    if node:
+                        if node.left:
+                            queue.append(node.left)
+                        if node.right:
+                            queue.append(node.right)
+                maxLength += 1
+            return maxLength
+
+
+        actualMax = 0
+        queue = deque([root])
+        while(queue):
+            node = queue.popleft()
+            if node:
+                queue.append(node.left)
+                queue.append(node.right)
+                if node.right:
+                    maxLengthOfRightSubTree = getMaxLengthOfSubTree(node.right)
+                else:
+                    maxLengthOfRightSubTree = 0
+                if node.left:
+                    maxLengthOfLeftSubTree = getMaxLengthOfSubTree(node.left)
+                else:
+                    maxLengthOfLeftSubTree = 0
+                actualMax = max(actualMax, maxLengthOfRightSubTree + maxLengthOfLeftSubTree)
+        return actualMax
