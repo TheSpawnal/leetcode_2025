0e139ee458a06283c62ab3e75d1d617a05700148
ALJDBS <70330110+TheSpawnal@users.noreply.github.com>
2025-10-19 22:08:00 +0200

    most_common methods py

--- amazon/819_most_common_word.py
+++ amazon/819_most_common_word.py
@@ -50,5 +50,17 @@ class Solution:
 # The space used by the banned set is also O(m), as it contains 
 # the unique banned words.
 
+#more pragmatic 70% faster at least: 
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        s= re.split(r"[^\w]+", paragraph.lower())
+        hash_map=Counter(s)
+        for i,j in hash_map.most_common():
+            if i in banned or i == "":
+                continue
+            else:
+                print(i)
+                return i
+        
 
 
