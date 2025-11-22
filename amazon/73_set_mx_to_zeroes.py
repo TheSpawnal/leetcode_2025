578b986aa03119d319dbaaeff8d9533999933b14
ALJDBS <70330110+TheSpawnal@users.noreply.github.com>
2025-11-22 12:35:39 +0100

    Update 73_set_mx_to_zeroes.py

--- amazon/73_set_mx_to_zeroes.py
+++ amazon/73_set_mx_to_zeroes.py
@@ -23,7 +23,73 @@ Follow up:
 
 '''
 
+#bitmask
 class Solution:
     def setZeroes(self, matrix: List[List[int]])-> None:
       #do not return anything, modify mx in place instead
-      
+        n,m = len(matrix), len(matrix[0])
+        col0, row0 = 0, 0
+        for i, row in enumerate(matrix):
+            for j, x in enumerate(row):
+                if x == 0:
+                    row0 |= (1<<i)
+                    col0 |=(1<<j)
+        for i in range(n):
+            if(row0 >> i) &1:
+                for j in range(m):
+                    matrix[i][j]=0
+        for j in range(m):
+            if (col0 >> j) &1:
+                for i in range(n):
+                    matrix[i][j]=0
+
+
+#hashset
+class Solution:
+    def setZeroes(self, matrix: List[List[int]])-> None:
+      #do not return anything, modify mx in place instead
+        n,m = len(matrix), len(matrix[0])
+        col0, row0 =set(),set()
+        for i, row in enumerate(matrix):
+            for j, x in enumerate(row):
+                if x == 0:
+                    row0.add(i)
+                    col0.add(j)
+        for i in row0:
+                for j in range(m):
+                    matrix[i][j]=0
+        for j in col0:
+                for i in range(n):
+                    matrix[i][j]=0
+
+#in place solution:
+class Solution:
+    def setZeroes(self, matrix, List[List[int]])->None:
+    n,m = len(matrix), len(matrix[0])
+    row0 = any(x == 0 for x in matrix[0])
+    col0 = any(x[0] == 0 for x in matrix)
+
+    for i in range(1,n):
+        for j in range (1,m):
+            if matrix[i][j] =0:
+                matrix[i][0]=0
+                matrix[0][j]=0
+    for i in range(1,n):
+        if matrix[i][0] == 0:
+            for j in range(m):
+                matrix[i][j] = 0
+    for j in range(1,m):
+            if matrix[0][j] == 0:
+                for i in range(n):
+                    matrix[i][j] =0
+    if row0:
+        for j in range(m):
+            matrix[0][j] = 0
+    if col0:
+        for i in range(n):
+            matrix[i][0] = 0
+
+
+
+
+
