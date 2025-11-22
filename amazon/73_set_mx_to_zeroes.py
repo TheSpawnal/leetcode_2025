6b3d00222e901c32ed1c0bd67fea52e03e572996
ALJDBS <70330110+TheSpawnal@users.noreply.github.com>
2025-11-22 11:43:05 +0000

    Update 73_set_mx_to_zeroes.py

--- amazon/73_set_mx_to_zeroes.py
+++ amazon/73_set_mx_to_zeroes.py
@@ -64,32 +64,30 @@ class Solution:
 
 #in place solution:
 class Solution:
-    def setZeroes(self, matrix, List[List[int]])->None:
-    n,m = len(matrix), len(matrix[0])
-    row0 = any(x == 0 for x in matrix[0])
-    col0 = any(x[0] == 0 for x in matrix)
+    def setZeroes(self, matrix: List[List[int]])->None:
+        n,m = len(matrix), len(matrix[0])
+        row0 = any(x == 0 for x in matrix[0])
+        col0 = any(x[0] == 0 for x in matrix)
 
-    for i in range(1,n):
-        for j in range (1,m):
-            if matrix[i][j] =0:
-                matrix[i][0]=0
-                matrix[0][j]=0
-    for i in range(1,n):
-        if matrix[i][0] == 0:
+        for i in range(1,n):
+            for j in range (1,m):
+                if matrix[i][j] ==0:
+                    matrix[i][0]=0
+                    matrix[0][j]=0
+        for i in range(1,n):
+            if matrix[i][0] == 0:
+                for j in range(m):
+                    matrix[i][j] = 0
+        for j in range(1,m):
+                if matrix[0][j] == 0:
+                    for i in range(n):
+                        matrix[i][j] =0
+        if row0:
             for j in range(m):
-                matrix[i][j] = 0
-    for j in range(1,m):
-            if matrix[0][j] == 0:
-                for i in range(n):
-                    matrix[i][j] =0
-    if row0:
-        for j in range(m):
-            matrix[0][j] = 0
-    if col0:
-        for i in range(n):
-            matrix[i][0] = 0
-
-
+                matrix[0][j] = 0
+        if col0:
+            for i in range(n):
+                matrix[i][0] = 0
 
 
 
